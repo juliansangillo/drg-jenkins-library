aa8c0767591620490496ce0bc20c56eaab2f58e2
Julian Sangillo <juliansangillo@gmail.com>
2020-11-18 19:56:02 -0500

    fix: bad variable references

--- vars/parallelize.groovy
+++ vars/parallelize.groovy
@@ -1,9 +1,9 @@
 def call(String labelPrefix, String[] axisValues, Closure execute) {
     
     def tasks = [:]
-    for(int i = 0; i < axis.size(); i++) {
-        def axisValue = axis[i]
-        def label = prefix + '-' + i
+    for(int i = 0; i < axisValues.size(); i++) {
+        def axisValue = axisValues[i]
+        def label = labelPrefix + '-' + i
         tasks[axisValue] = {
             stage(name) {
                 node(label) {
