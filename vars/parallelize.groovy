f385a50b958631efff4b2cd0f4121736aab2451b
Julian Sangillo <juliansangillo@gmail.com>
2020-11-24 16:38:38 -0500

    feat: Moved AXIS_NAME into stage with intermediate variable

--- vars/parallelize.groovy
+++ vars/parallelize.groovy
@@ -2,10 +2,11 @@ def call(String label, String[] axisList, Closure execute) {
     
     def tasks = [:]
     for(int i = 0; i < axisList.size(); i++) {
-        env.AXIS_NAME = axisList[i]
-        tasks[env.AXIS_NAME] = {
-            stage(env.AXIS_NAME) {
+        def axisName = axisList[i]
+        tasks[axisName] = {
+            stage(axisName) {
                 node(label) {
+                    env.AXIS_NAME = axisName
                     execute()
                 }
             }
