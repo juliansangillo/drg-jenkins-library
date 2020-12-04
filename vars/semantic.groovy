32a7299df7acd4de8d8f56e32b039e463927a49b
Julian Sangillo <juliansangillo@gmail.com>
2020-12-03 19:28:26 -0500

    feat: Added semantic.version to get next releasable version

---
+++ vars/semantic.groovy
@@ -0,0 +1,15 @@
+def version(String githubToken) {
+
+    env.GITHUB_TOKEN = githubToken
+
+    def version = sh (
+        script: "semantic-release -d | grep -oP 'Published release \K.*? ' | xargs",
+        returnStdout: true
+    )
+
+    return version
+} 
+
+def release() {
+
+}
