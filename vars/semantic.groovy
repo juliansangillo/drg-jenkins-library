470c23819037aa2c16fdcd055ebaba41536f420a
Julian Sangillo <juliansangillo@gmail.com>
2020-12-03 21:21:55 -0500

    feat: semantic.init creates releaserc config file

--- vars/semantic.groovy
+++ vars/semantic.groovy
@@ -1,3 +1,22 @@
+def init(String prodBranch, String testBranch, String devBranch, String prodIsPrerelease, String testIsPrerelease, String devIsPrerelease, String changelogFileName, String changelogTitle) {
+
+    def releaserc = libraryResource('com/deltarenegadegames/unityci/releaserc.json')
+    
+    releaserc = releaserc.replaceAll('%PROD_BRANCH%', prodBranch)
+    releaserc = releaserc.replaceAll('%TEST_BRANCH%', testBranch)
+    releaserc = releaserc.replaceAll('%DEV_BRANCH%', devBranch)
+    releaserc = releaserc.replaceAll('%PROD_IS_PRERELEASE%', prodIsPrerelease)
+    releaserc = releaserc.replaceAll('%TEST_IS_PRERELEASE%', testIsPrerelease)
+    releaserc = releaserc.replaceAll('%DEV_IS_PRERELEASE%', devIsPrerelease)
+    releaserc = releaserc.replaceAll('%CHANGELOG_FILE_NAME%', changelogFileName)
+    releaserc = releaserc.replaceAll('%CHANGELOG_TITLE%', changelogTitle)
+    
+    File releasercFile = new File('.releaserc')
+    releasercFile.write releaserc
+
+    println releasercFile.text
+}
+
 def version(String githubToken) {
 
     env.GITHUB_TOKEN = githubToken
