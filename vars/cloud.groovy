a850532e77e8928c20b0589d52d82d3115f4d55e
Julian Sangillo <juliansangillo@gmail.com>
2020-12-08 20:52:38 -0500

    feat: cloud.uncache added Google Storage Download steps

--- vars/cloud.groovy
+++ vars/cloud.groovy
@@ -1,9 +1,28 @@
- def login(String cloudProject, String jenkinsCredentialsId) {
-    withCredentials([file(credentialsId: jenkinsCredentialsId, variable: 'SA_KEY')]) {
-        sh (
-          script: "gcloud auth activate-service-account jenkins@unity-firebuild.iam.gserviceaccount.com --key-file=${SA_KEY} --project=${cloudProject}",
-          label: 'Google Cloud Activate Service Account'
-        )
-    }
- }
+def login(String cloudProject, String jenkinsCredentialsId) {
+   withCredentials([file(credentialsId: jenkinsCredentialsId, variable: 'SA_KEY')]) {
+       sh (
+         script: "gcloud auth activate-service-account jenkins@unity-firebuild.iam.gserviceaccount.com --key-file=${SA_KEY} --project=${cloudProject}",
+         label: 'Google Cloud Activate Service Account'
+       )
+   }
+}
+ 
+def uncache(String cacheBucket, String jobName, String platform, String projectPath) {
+   echo 'Pulling from cache ...'
+   
+   def objects = sh(
+       script: "gsutil ls -l 'gs://${cacheBucket}/${jobName}/${platform}/'",
+       label: "Google Storage List Objects BUCKET=${cacheBucket} JOB=${jobName} PLATFORM=${platform}",
+       returnStdout: true
+   )
+   
+   objects.split(' ').each{ obj ->
+       sh (
+           script: "gsutil -m -q cp -r \"${obj}\" \"${projectPath}\""
+           label: 'Google Storage Download'
+       )
+   }
+   
+   echo 'Cache pulled successfully'
+}
  
