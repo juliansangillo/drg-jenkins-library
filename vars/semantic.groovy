4b9c54a00cc3d281f4245da115742ffdaa6385e7
Julian Sangillo <juliansangillo@gmail.com>
2020-12-14 11:38:54 -0500

    fix: Potential: rollback not working as expected on Jenkins

--- vars/semantic.groovy
+++ vars/semantic.groovy
@@ -49,11 +49,13 @@ def release(String githubCredentialsId) {
                     OWNER=$(cat .git/config | grep "url" | grep -oP "https://github.com/\\K.*/" | tr -d '/');
                     REPO=$(cat .git/config | grep "url" | grep -oP "https://github.com/.*/\\K.*." | tr -d '.git');
                 
-                    RELEASE_ID=$(curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/releases/tags/v$VERSION" | jq -r '.id');
+                    RELEASE_JSON="$(curl -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/releases/tags/v$VERSION")";
+                    RELEASE_ID=$(echo "$RELEASE_JSON" | jq -r '.id')
                 
                     curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/releases/$RELEASE_ID";
                 
-                    git config user.name "$OWNER";
+                    git remote rm origin;
+                    git remote add origin "git@github.com:$OWNER/$REPO.git";
                     git config user.password "$GITHUB_TOKEN";
                     git tag -d v$VERSION;
                     git push origin :v$VERSION
