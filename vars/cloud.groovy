088fbbc95115c4394f3a64dc8d6b1ab395547827
Julian Sangillo <juliansangillo@gmail.com>
2021-05-20 16:28:23 -0400

    fix(cloud): deploying to run

--- vars/cloud.groovy
+++ vars/cloud.groovy
@@ -78,23 +78,20 @@ def deployToRun(String serviceName, String region, String imageName, String vers
         }
     }
     
-    sh (
-        script: """
-            gcloud run deploy ${serviceName} --platform=managed \
-                --region=${region} \
-                --image=${imageName}:${version} \
-                --set-env-vars=${envVars} \
-                --port=${port} \
-                --service-account=${serviceAccount} \
-                --memory=${memory} \
-                --cpu=${cpu} \
-                --timeout=${timeout} \
-                --concurrency=${maximumRequests} \
-                --max-instances=${maxInstances} \
-                ${db_config} \
-                ${vpc_connector} \
-                ${vpc_egress}
-        """,
-        returnStatus: true
-    )
+    sh """
+        gcloud run deploy ${serviceName} --platform=managed \
+            --region=${region} \
+            --image=${imageName}:${version} \
+            --set-env-vars=${envVars} \
+            --port=${port} \
+            --service-account=${serviceAccount} \
+            --memory=${memory} \
+            --cpu=${cpu} \
+            --timeout=${timeout} \
+            --concurrency=${maximumRequests} \
+            --max-instances=${maxInstances} \
+            ${db_config} \
+            ${vpc_connector} \
+            ${vpc_egress}
+    """
 }
